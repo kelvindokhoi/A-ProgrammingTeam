3237d919767013d43325fe877d00af4005e4866b
Nguyen Do <konu0315@gmail.com>
2024-12-05 11:54:26 -0500

    modified:   TheNthDayofChrismas.py

--- TheNthDayofChrismas.py
+++ TheNthDayofChrismas.py
@@ -1,1 +1,24 @@
+'''
+The N Days of Chrismas
 print(f'{(m:=sum(range(n:=int(input())+1)))}\n{m*(n+1)//3}')
+
+this is ASR: >>
+
+
+a=int(input())
+print(b:=a*a+a>>1)
+print(b*(a+2)//3)
+
+a=int(input())
+b=a*a+a>>1
+print(b)
+print((a|2)*b//3)
+
+##Shortest n lines
+a=int(input())
+print(b:=a*a+a>>1,b*(a+2)//3)
+
+##Shortest 1 line:
+'''
+
+print(b:=(a:=int(input()))*a+a>>1,b*(a+2)//3)
