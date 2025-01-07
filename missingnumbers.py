28454de291a2616d9c5ae4a8c8c2e1d25e39d84e
Nguyen Do <konu0315@gmail.com>
2025-01-06 19:20:24 -0500

    Changes to be committed: 	modified:   A.TEST.py 	modified:   missingnumbers.py

--- missingnumbers.py
+++ missingnumbers.py
@@ -1,2 +1,12 @@
 # missingnumbers
 
+a=[*map(int,open(0))];print(*({*range(1,-~a[-1])}-{*a[1:]}or['good job']),sep='\n')
+
+# i,j=int,input;x={i(j())for m in' '*~-i(j())}
+# c=sorted([*({*range(1,i(j()))}-x)])
+# print(["\n".join(c),'good job'])[c==[]]
+
+# *a,=map(int,open(0).read().split())
+# b={*range(1,-~a[-1])}-{*a[1::]}
+# print(*(b or['good job']),sep='\n')
+
