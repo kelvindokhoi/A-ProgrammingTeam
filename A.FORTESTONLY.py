16128894f606a4a04f30d588f229da838990dce3
Nguyen Do <konu0315@gmail.com>
2025-02-11 10:32:01 -0500

    Changes to be committed: 	modified:   A.FORTESTONLY.py 	new file:   UVA Problems/10131.py 	new file:   UVA Problems/10131_sample_in.txt 	new file:   UVA Problems/A.testonly.py

--- A.FORTESTONLY.py
+++ A.FORTESTONLY.py
@@ -1,10 +1,3 @@
-i,j=int,input;from collections import deque as d
-def x(z,l,r=0):
- for c in z:
-  if'R'==c:r^=1
-  else:
-   if not l:return"error"
-   [l.pop()if r else l.popleft()]
- if r:l.reverse()
- return'['+','.join(map(str,l))+']'
-exec("z,_=j(),j();l=j()[1:-1].split(',');l=d()if l==['']else d(map(i,l));print(x(z,l));"*i(j()))
+a=[1,2,3,4]
+for i in a[::2]:
+    print(i)
---
+++ UVA Problems/10131.py
@@ -0,0 +1,27 @@
+# 10131
+# Is Bigger Smarter?
+
+
+*f,=map(int,open('10131_sample_in.txt').read().split()) #Debugging line, the code starts from below
+# import sys
+# f = sys.stdin.read().split()
+def keyE(elephant):return elephant['weight']*1e6+elephant['iq']
+n = len(f) // 2
+elehehe = [{'name': i + 1, 'weight': int(f[2 * i]), "iq":int(f[2 * i + 1])} for i in range(n)]
+elehehe.sort(key=keyE)
+DynamicElephants = [1]*n
+TroopOfElephants = [[]for _ in range(n)]
+best = []
+max_len = 1
+for i in range(n-1,-1,-1):
+    TroopOfElephants[i]=[elehehe[i]['name']]
+    for j in range(i+1,n):
+        if elehehe[i]['iq']>elehehe[j]['iq']:
+            if DynamicElephants[j]+1>DynamicElephants[i]:
+                DynamicElephants[i]=DynamicElephants[j]+1
+                TroopOfElephants[i]=[elehehe[i]['name']]+TroopOfElephants[j]
+            if DynamicElephants[i]>max_len:
+                max_len=DynamicElephants[i]
+                best = TroopOfElephants[i]
+print(max_len)
+print(*best,sep="\n")
