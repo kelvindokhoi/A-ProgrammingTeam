4384f0afa347a04b3d4a55463c67712a441d82fd
Nguyen Do <konu0315@gmail.com>
2025-03-11 23:11:46 -0400

    Changes to be committed: 	modified:   A.FORTESTONLY.py 	modified:   Climbing_Worm.py 	new file:   climbingworm_in.txt

--- A.FORTESTONLY.py
+++ A.FORTESTONLY.py
@@ -1,4 +1,1 @@
-h,w,n,*b=map(int,open(0).read().split());q,r,z=[0]*h,0,1
-for k in b:
- if r<h:q[r]+=k;z&=q[r]<=w;r+=q[r]==w
-print(['NO','YES'][(q[-1]==w)*z])
+a,b,c=map(int,input().split());print(+(a>c)or(c+~a)//(a-b)+2)
--- Climbing_Worm.py
+++ Climbing_Worm.py
@@ -1,3 +1,12 @@
 # Climbing Worm
 # climbingworm
 
+up,down,total = map(int,input().split())
+one_cycle = up-down
+num_cycle = (total-up)//one_cycle
+if up>=total:
+    print(1)
+else:
+    print(num_cycle+1)
+
+# a,b,c=map(int,input().split());print(+(a>c)or(c+~a)//(a-b)+2)
