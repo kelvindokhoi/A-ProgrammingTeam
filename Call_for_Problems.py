3c9cdee2e4bb3d74e2a967f825962bc61f6d2132
Nguyen Do <konu0315@gmail.com>
2025-03-24 11:21:13 -0400

    Changes to be committed: 	new file:   Call_for_Problems.py 	new file:   Programmeringsolympiadens_Budget.py

---
+++ Call_for_Problems.py
@@ -0,0 +1,3 @@
+# Call for Problems
+
+print(sum(int(input())&1 for i in' '*int(input())))
---
+++ Programmeringsolympiadens_Budget.py
@@ -0,0 +1,6 @@
+# Programmeringsolympiaden's Budget
+
+n = 0
+for i in' '*int(input()):
+    input();n+=int(input())
+print("Usch, vinst"if n>0 else"Lagom"if n==0 else"Nekad")
