802dcab0d343cc71f8f3201f8afd8dcd2a50df5b
Nguyen Do <konu0315@gmail.com>
2025-03-26 11:07:01 -0400

    Changes to be committed: 	new file:   Bits.py 	new file:   Connect_the_Dots.py 	new file:   bits_in.txt

---
+++ Bits.py
@@ -0,0 +1,8 @@
+# Bits
+# bits
+
+# for _ in' '*int(input()):
+#     curr = ''
+#     print(max(bin(int(curr:=curr+new)).count('1')for new in input()))
+
+exec("c='';print(max(bin(int(c:=c+n)).count('1')for n in input()));"*int(input()))
